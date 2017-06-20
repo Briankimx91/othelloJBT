89121e963a105dace1fbc03f5c57c45a0c6455c3
Brian <briankimx91@gmail.com>
2017-06-19 22:37:50 -0700

    created divs using jquery and touched up a little bit of html and css

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var something;
+
+
+
+$(document).ready(function(){
+    generateSpots();
+});
+function generateSpots(){
+    for (var i = 1; i < 9; i++) {
+        $('#back-board').append('<div id="row' + i + '"></div>');
+    }
+    for(var k = 1; k < 9; k++){
+        $("#row1").append("<div></div>");
+        $("#row2").append("<div></div>");
+        $("#row3").append("<div></div>");
+        $("#row4").append("<div></div>");
+        $("#row5").append("<div></div>");
+        $("#row6").append("<div></div>");
+        $("#row7").append("<div></div>");
+        $("#row8").append("<div></div>");
+    }
+}
 function Game(){
     
 
