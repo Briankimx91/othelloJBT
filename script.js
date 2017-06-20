8fb78469b258a7c518548c8071db645a2edeb63a
Jay Lim <jaylim106@gmail.com>
2017-06-20 09:35:58 -0700

    minor changes

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(createObj);
 
 function createObj(){
     generateSpots();
-    // var gameObj = new Game();
-    // gameObj.init();
+    var gameObj = new Game();
+    gameObj.init();
 }
 
 var something;
@@ -19,58 +19,58 @@ function generateSpots(){
     }
 }
 
-// function Game(){
-//     this.self = this;
-//     this.num_black = null;
-//     this.num_white =null;
-//     // implement this later on
-//     // this.timer_value = 30000;
-//     // this.timer_mode = false;
-//     this.player_list = ["player 1","player 2"];
-//     this.turn = null;
-//     this.winner = null;
-//     //functions down here
-//
-//
-//     this.init(){
-//         this.turn = this.player_list[0];
-//     }
-//
-//     this.legalmoves(){
-//
-//
-//         return array; <-- if(array.length==0)
-//                             you suck func;
-//                             return;
-//     }
-//
-//     this.clickhandler(){
-//         if this.turn = black
-//             then go white
-//         else
-//             go black
-//         flip();
-//     }
-//
-//
-//
-//     this.hover//for legal moves
-//                 //show which discs would flip
-//
-//
-//
-//
-//     this.flip(){ //some parameters
-//
-//     }
-//
-//     this.reset function(){
-//         this.turn = null;
-//         this.num_black =null;
-//         this.num_white = null;
-//         this.winner = null;
-//
-//     }
-//
-// }
-//
+function Game(){
+    this.self = this;
+    this.num_black = null;
+    this.num_white =null;
+    // implement this later on
+    // this.timer_value = 30000;
+    // this.timer_mode = false;
+    this.player_list = ["player 1","player 2"];
+    this.turn = null;
+    this.winner = null;
+    //functions down here
+
+
+    this.init(){
+        this.turn = this.player_list[0];
+    }
+
+    // this.legalmoves(){
+    //
+    //
+    //     return array; <-- if(array.length==0)
+    //                         you suck func;
+    //                         return;
+    // }
+    //
+    // this.clickhandler(){
+    //     if this.turn = black
+    //         then go white
+    //     else
+    //         go black
+    //     flip();
+    // }
+    //
+    //
+    //
+    // this.hover//for legal moves
+    //             //show which discs would flip
+    //
+    //
+    //
+    //
+    // this.flip(){ //some parameters
+    //
+    // }
+    //
+    // this.reset function(){
+    //     this.turn = null;
+    //     this.num_black =null;
+    //     this.num_white = null;
+    //     this.winner = null;
+    //
+    // }
+
+}
+
